47f2adbac512a4c51c8a0b9365e7405c6f81d26a
oblin99 <rycobb@gmail.com>
2026-04-06 15:04:52 -0400

    Add reference menu

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3099788197" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3004917518" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
